b35327798e44012aab84a778d811638a9a86b8b7
nlsdfnbch <23okrs35+github@mykolab.com>
2016-02-19 09:02:02 +0100

    removed logging facility from example;

--- example.py
+++ example.py
@@ -1,18 +1,4 @@
-import logging
 from btcelib.Kraken import Kraken
-
-logging.basicConfig(level=logging.DEBUG,
-                    datefmt='%m-%d %H:%M',
-                    filename='kraken_returns.log',
-                    filemode='w+')
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s %(name)-4s: %(levelname)-4s %(message)s')
-console.setFormatter(formatter)
-
-log = logging.getLogger()
-
 
 kraken = Kraken()
 
